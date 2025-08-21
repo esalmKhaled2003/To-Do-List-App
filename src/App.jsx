4de200a5bf311396862567905e4cad68f9268eba
Eslam Khaled <ealamk646@gmail.com>
2025-08-21 08:22:34 +0300

    day 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
-import React from "react";
+// @ts-nocheck
+import React, { useState } from "react";
 import Title from "./Compontents/Title";
 import Form from "./Compontents/Form";
 import ListItems from "./Compontents/ListItems";
 
+
+
 function App() {
+  const [items , setItems] = useState([]);
+  const [description,setDescription] = useState("");
+
+  function handelAddItem(item) {
+    setItems((prevItems) => [...prevItems, item]);
+  }
+
+  console.log(items);
+
   return (
     <main id="main">
       <div className="container">
         <div className="todo-list">
           <Title />
-          <Form />
+          <Form description={description} setDescription={setDescription} 
+           onAddItem={handelAddItem}/>
           <ListItems />
         </div>
       </div>
--- src/Compontents/Form.jsx
+++ src/Compontents/Form.jsx
@@ -1,12 +1,34 @@
-import React from 'react'
+import React from "react";
 
-function Form() {
+function Form({ description, setDescription , onAddItem}) {
+  function handelSubmit(e){
+    e.preventDefault();
+
+    if(description === ""){
+      return alert("You Must Add A Value!")
+    }
+    
+    const newItem = {
+      id: Date.now(),
+      description,
+      seleted: false,
+    };
+
+    onAddItem(newItem);
+  }
   return (
-    <form className='form'>
-        <input type="text" placeholder='Add a new task' />
-        <button className='btn-add'>Add</button>
+    <form className="form" onSubmit={handelSubmit}>
+      <div className="form-control">
+        <input
+          type="text"
+          placeholder="Add a new task"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <button className="btn-add">Add</button>
+      </div>
     </form>
-  )
+  );
 }
 
 export default Form;
